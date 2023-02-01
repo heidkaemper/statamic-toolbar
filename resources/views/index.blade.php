49dd5c3e716218e93a1cda196c5e71c5e8cc0b1d
Lars Heidkämper <heidkaemper@new-data-services.de>
2023-02-01 11:34:11 +0100

    use nocache

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,3 @@
 <link rel="stylesheet" href="/vendor/statamic-toolbar/toolbar.css">
 
-<div id="statamic_toolbar" @class(['debugbar-enabled' => config('debugbar.enabled')])>
-    @includeWhen(config('statamic.toolbar.components.breakpoint'), 'statamic-toolbar::components.breakpoint')
-
-    @includeWhen(config('statamic.toolbar.components.site'), 'statamic-toolbar::components.site')
-
-    @includeWhen(config('statamic.toolbar.components.template'), 'statamic-toolbar::components.template')
-
-    @includeWhen(config('statamic.toolbar.components.cp_link'), 'statamic-toolbar::components.cp_link')
-</div>
+@nocache('statamic-toolbar::toolbar')
---
+++ resources/views/toolbar.blade.php
@@ -0,0 +1,9 @@
+<div id="statamic_toolbar" @class(['debugbar-enabled' => config('debugbar.enabled')])>
+    @includeWhen(config('statamic.toolbar.components.breakpoint'), 'statamic-toolbar::components.breakpoint')
+
+    @includeWhen(config('statamic.toolbar.components.site'), 'statamic-toolbar::components.site')
+
+    @includeWhen(config('statamic.toolbar.components.template'), 'statamic-toolbar::components.template')
+
+    @includeWhen(config('statamic.toolbar.components.cp_link'), 'statamic-toolbar::components.cp_link')
+</div>
