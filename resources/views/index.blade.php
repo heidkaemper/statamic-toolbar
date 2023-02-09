64f6a4e7206899ea79ac981a3946677fc579fb77
Lars Heidkämper <heidkaemper@new-data-services.de>
2023-02-09 21:45:47 +0100

    add version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,3 @@
-<script src="/vendor/statamic-toolbar/toolbar.js" defer></script>
+<script src="/vendor/statamic-toolbar/toolbar.js?{{ $version }}" defer></script>
 
 <statamic-toolbar endpoint="{{ $endpoint }}"></statamic-toolbar>
--- src/Toolbar.php
+++ src/Toolbar.php
@@ -2,6 +2,7 @@
 
 namespace Heidkaemper\Toolbar;
 
+use Composer\InstalledVersions;
 use Symfony\Component\HttpFoundation\Response;
 
 class Toolbar
@@ -21,9 +22,19 @@ class Toolbar
 
         $widget = view('statamic-toolbar::index', [
             'endpoint' => route('statamic-toolbar-endpoint', [], false),
+            'version' => $this->getVersion(),
         ]);
 
         $response->setContent(mb_substr($content, 0, $pos) . $widget . mb_substr($content, $pos));
         $response->headers->remove('Content-Length');
+    }
+
+    protected function getVersion(): string
+    {
+        if (! InstalledVersions::isInstalled('heidkaemper/statamic-toolbar')) {
+            return '';
+        }
+
+        return InstalledVersions::getPrettyVersion('heidkaemper/statamic-toolbar');
     }
 }
